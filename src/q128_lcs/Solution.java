09d3b5bde91ca9cb9bf1afe918b8dcf3909f7ac7
unknown <YangTing@.(none)>
2013-07-04 23:16:01 +0800

    add new question

---
+++ src/q128_lcs/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Create date 2013-7-4
+ *
+ * @author YangTing
+ *
+ */
+
+
+package q128_lcs;
+
+public class Solution {
+    public int longestConsecutive(int[] num) {
+      
+    	return 0;
+    }
+}
