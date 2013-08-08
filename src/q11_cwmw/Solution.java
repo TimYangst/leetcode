b10320ceaf4f454f8ba608cd94c56add464004a6
TimYangst <yt0701@gmail.com>
2013-08-08 20:23:54 +0800

    add q11 package unslove

---
+++ src/q11_cwmw/Solution.java
@@ -0,0 +1,46 @@
+package q11_cwmw;
+
+public class Solution {
+
+/*
+	class Element
+	{
+		int height;
+		int index;
+		int rank;
+	}
+	public int maxArea(int[] height) {
+		
+		Element[] eles = new Element[height.length];
+		for (int i = 0; i < height.length ; i++)
+		
+		
+		
+		//int f[] = new int[height.length + 1];
+		
+		
+		int maxhigh[] = new int[height.length + 1];
+		int indicate[] = new int[height.length + 1];
+		
+		int sum[] = new int[height.length + 1];
+		
+		
+		maxhigh[0] =  0;
+		indicate[0] = 0;
+		sum[0]  = 0;
+		
+		
+		for (int  i = 1; i < height.length; i++)
+		{
+			sum[i] = sum[i-1] + height[i-1];
+			
+			int j = i;
+			while (j <= height.length)
+			{
+				if ()
+			}
+			
+		}
+		return 0;
+	}*/
+}
