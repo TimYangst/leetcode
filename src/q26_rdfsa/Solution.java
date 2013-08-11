b8e7d2cb2b087c317dc9ee5a6e8d2926f7441cd4
TimYangst <yt0701@gmail.com>
2013-08-11 23:15:27 +0800

    update to 35

---
+++ src/q26_rdfsa/Solution.java
@@ -0,0 +1,32 @@
+package q26_rdfsa;
+
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        int length = A.length;
+        int i = 0;
+        int j = 0;
+        int k,deta;
+        while (i < length)
+        {
+        	j = i;
+        	while (j<length && A[j] == A[i] )
+        	{
+        		j++;
+        	}
+        	
+        	deta = j - i -1;
+        	i++;
+        	if (deta > 0)
+        	{
+        		k = i;
+        		length -= deta;
+        		while (k < length)
+        		{
+        			A[k] = A[k+deta];
+        			k++;
+        		}
+        	}
+        }
+        return length;
+    }
+}
---
+++ src/q32_lvp/Solution.java
@@ -0,0 +1,31 @@
+package q32_lvp;
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+    	int rst = 0;
+    	int[] f = new int[s.length() + 1];
+    	for (int i = 0; i<= s.length() ; i++) f[i] = 0;
+    	for (int  i = s.length() - 2; i>=0 ;i--)
+    	{
+    		if (s.charAt(i) == '(')
+    		{
+    			int j = i +1;
+    			int k = 1;
+    			while (j < s.length()- k + 1 && k > 0)
+    			{
+    				if (s.charAt(j) == ')') k--;
+    				else k ++;
+    				j ++;
+    			}
+    			if (k == 0)
+    			{
+    				f[i] = f[j] + j - i;
+    				if (f[i] > rst) rst = f[i];
+    			}
+    			
+    		}
+    		
+    	}
+    	return rst ;
+    }
+}
