d8132d44ecd64e6223d6336319db1f86cd3e104e
Ting Yang <t-tiyan@microsoft.com>
2013-08-15 17:00:28 +0800

    update to 68

---
+++ src/q59_smII/Solution.java
@@ -0,0 +1,26 @@
+package q59_smII;
+
+public class Solution {
+	  public int[][] generateMatrix(int n) {
+		  int[][] rst = null;
+		  if (n ==0){rst = new int[0][]; return rst;};
+		  if (n ==1){rst = new int[1][]; rst[0]= new int[1]; rst[0][0] = 1; return rst;}
+		  if (n > 1) {
+			  rst = new int[n][];
+			  for (int i =0 ;i<n;i++) rst[i] = new int[n];
+			  int l=0,r=n-1,u=0,d=n-1;
+			  int k =1;
+			  while (l < r)
+			  {
+				  for (int j = l; j < r; j++) rst[u][j] = k++;
+				  for (int i = u; i < d; i++) rst[i][r] = k++;
+				  for (int j = r; j > l; j--) rst[d][j] = k++;
+				  for (int i = d; i > u; i--) rst[i][l] = k++;
+				  l ++ ; r--;
+				  u ++ ; d--;
+			  }
+			  if (l==r) rst[u][l] = k++;
+		  }
+		  return rst;
+	  }
+}
---
+++ src/q64_mps/Solution.java
@@ -0,0 +1,26 @@
+package q64_mps;
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+    	int[][] f;
+    	if (grid.length == 0) return 0;
+    	if (grid[0].length == 0) return 0;
+    	
+    	f =  new int[grid.length][];
+    	for (int i = 0 ; i<  f.length; i++)
+    	{
+    		f[i] =  new int[grid[0].length];
+    	}
+    	for (int i = 0 ; i<  f.length; i++)
+    		for (int  j =0 ;j < f[0].length; j++)
+    		{
+    			f[i][j] = grid[i][j];
+    			if ( i == 0 && j == 0) continue;
+    			int min =  Integer.MAX_VALUE;
+    			if ( i > 0 && f[i-1][j] < min) min = f[i-1][j];
+    			if ( j > 0 && f[i][j-1] < min) min = f[i][j-1];
+    			f[i][j] += min;
+    		}
+        return f[f.length-1][f[0].length-1];
+    }
+}
