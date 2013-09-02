ae8e73cb84244d7896951dd6aac97385bb1e75e7
TimYangst <yt0701@gmail.com>
2013-09-02 21:40:03 +0800

    add cpp project

---
+++ leetcode/4Sum.cpp
@@ -0,0 +1,13 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+		vector<vector<int>> rst ;
+		if (num.size() < 4) return rst;
+
+		return rst;
+    }
+};
